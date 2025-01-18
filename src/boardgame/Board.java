e06d2c05e700bffe91b81e9fe20c9050f2c84525
João Paulo Fontana Macedo <jpfontana12@icloud.com>
2025-01-18 12:44:19 -0300

    feat: chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -26,4 +26,12 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece pirce(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece pirce(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
