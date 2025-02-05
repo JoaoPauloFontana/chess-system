9c292c19c5a486b319162b997ac08f51d83647d4
João Paulo Fontana Macedo <jpfontana12@icloud.com>
2025-02-05 15:14:18 -0300

    feat: pawn

--- src/application/UI.java
+++ src/application/UI.java
@@ -121,12 +121,12 @@ public class UI {
 
         System.out.println("Captured pieces: ");
         System.out.print("White: ");
-        System.out.print(ANSI_RED);
+        System.out.print(ANSI_YELLOW);
         System.out.println(Arrays.toString(white.toArray()));
         System.out.print(ANSI_RESET);
 
         System.out.print("Black: ");
-        System.out.print(ANSI_YELLOW);
+        System.out.print(ANSI_RED);
         System.out.println(Arrays.toString(black.toArray()));
         System.out.print(ANSI_RESET);
     }
